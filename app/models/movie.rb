ec0029cbed41d71bcbbb660de328d5ffe4d17594
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-10-08 17:30:42 +0000

    Automatically backed up by Learn

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,14 +1,2 @@
 class Movie < ActiveRecord::Base
 end
-
-class CreateMovies < ActiveRecord::Migration
-  def change
-    create_table :movies do |t|
-      t.string :title
-      t.integer :release_date
-      t.string :director
-      t.string :lead
-      t.boolean :in_theaters
-    end 
-  end
-end
