61c3d919297cfcbb1d752bbf08fe8bf8af0aee60
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-10-08 18:28:11 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_08_182032) do
+ActiveRecord::Schema.define(version: 2019_10_02_003841) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
